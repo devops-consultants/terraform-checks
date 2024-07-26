3751c9b0c3722e748e35f94ba264dc10bf45837c
Rob Coward <rob.coward@devops-consultants.co.uk>
2024-07-26 09:58:00 +0100

    fix: Ensure the repo-level .terraform-docs.yml file is used when checking the docs

--- pipe.sh
+++ pipe.sh
@@ -26,6 +26,12 @@ enable_debug
 
 info "Running module tests for ${TF_MODULE_PATH}"
 
+TERRAFORM_DOCS_CONFIG_OPTS=""
+if [[ -f .terraform-docs.yml ]]; then
+  info "Found .terraform-docs.yml file, using it for terraform-docs configuration"
+  TERRAFORM_DOCS_CONFIG_OPTS="--config $(pwd)/.terraform-docs.yml"
+fi
+
 cd ${TF_MODULE_PATH}
 terraform init
 
@@ -77,7 +83,7 @@ fi
 if [[ "${RUN_DOCS}" == "true" ]]; then
   info "Checking module documentation"
   touch README.md && cp README.md README.md.new
-  run terraform-docs markdown table --output-file README.md.new . && diff -bw README.md README.md.new
+  run terraform-docs markdown table ${TERRAFORM_DOCS_CONFIG_OPTS} --output-file README.md.new . && diff -bw README.md README.md.new
 
   if [[ "${status}" == "0" ]]; then
     success "Success!"
